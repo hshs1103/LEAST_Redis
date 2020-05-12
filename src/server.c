8f06e8beda1e4166a23e90bcdf6e25d3ac7e5f02
hanseungsung <hssung@yonsei.ac.kr>
2020-05-12 17:10:22 +0900

    delete unnecessary code[loadDataFromDisk]

--- src/server.c
+++ src/server.c
@@ -3677,33 +3677,6 @@ void loadDataFromDisk(void) {
                 exit(1);
             }
         }
-
-//        if (rdbLoad(server.rdb_filename,&rsi) == C_OK) {
-//            serverLog(LL_NOTICE,"DB loaded from disk: %.3f seconds",
-//                (float)(ustime()-start)/1000000);
-//
-//            /* Restore the replication ID / offset from the RDB file. */
-//            if (server.masterhost &&
-//                rsi.repl_id_is_set &&
-//                rsi.repl_offset != -1 &&
-//                /* Note that older implementations may save a repl_stream_db
-//                 * of -1 inside the RDB file in a wrong way, see more information
-//                 * in function rdbPopulateSaveInfo. */
-//                rsi.repl_stream_db != -1)
-//            {
-//                memcpy(server.replid,rsi.repl_id,sizeof(server.replid));
-//                server.master_repl_offset = rsi.repl_offset;
-//                /* If we are a slave, create a cached master from this
-//                 * information, in order to allow partial resynchronizations
-//                 * with masters. */
-//                replicationCacheMasterUsingMyself();
-//                selectDb(server.cached_master,rsi.repl_stream_db);
-//            }
-//        }
-//        else if (errno != ENOENT) {
-//            serverLog(LL_WARNING,"Fatal error loading the DB: %s. Exiting.",strerror(errno));
-//            exit(1);
-//        }
     }
 }
 
